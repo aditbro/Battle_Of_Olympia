b1001f6e1fc39919c95b25fb4d009dab23f1c6ab
osynligare <nic.nicholas1907@outlook.com>
2017-11-14 21:46:23 +0700

    pulling

--- map.c
+++ map.c
@@ -3,86 +3,13 @@
 #include "map.h"
 #include <stdio.h>
 
-
-// ----------Constructor----------
-void createMap(MAP *M, int row, int col) {
-	MapBrsEff(*M) = row-1;
-	MapKolEff(*M) = col-1;
-	
-	for (int i = 0; i <= MapBrsEff(*M); i++) {
-		for (int j = 0; j <= MapKolEff(*M); j++) {
-			Build_Type(Build(*M, i, j)) = Nil;
-			Unit(*M, i, j).type = Nil;
-		}
-	}
-	
-}
-// membuat map berukuran baris row dan kolom col
-
-
 // ----------Output----------
-void printMap(MAP M) {
-	int i, j;
+void printMap(Map M) {
+    int i,j;
 
-	printf(" ");
-	for (j = 0; j <= MapKolEff(M); j++) {
-		printf("   %d", j);
-	}
-	printf("\n  ");
-
-	for (i = 0; i <= MapBrsEff(M); i++) {
-		for (j = 0; j <= MapKolEff(M); j++) {
-			printf("****");
-		}
-		printf("*\n  ");
-		for (j = 0; j <= MapKolEff(M); j++) {
-			printf("* ");
-			if (Build_Type(Build(M, i, j)) == Nil) {
-				printf("  ");
-			}
-			else {
-				if(Build_Owner(Build(M, i, j))==1){
-					print_red(Build_Type(Build(M, i, j)));
-					printf(" ");
-				}else if(Build_Owner(Build(M, i, j))==2){
-					print_blue(Build_Type(Build(M, i, j)));
-					printf(" ");
-				}else{
-					print_magenta(Build_Type(Build(M, i, j)));
-					printf(" ");
-				}
-			}
-		}
-		printf("*\n");
-		printf("%d ", i);
-		for (j = 0; j <= MapKolEff(M); j++) {
-			printf("* ");
-			if (Type(Unit(M, i, j)) == Nil) {
-				printf("  ");
-			}
-			else {
-				if (Owner(Unit(M, i, j)) == 1) {
-					print_red(Type(Unit(M, i, j)));
-					printf(" ");
-				}
-				else if (Owner(Unit(M, i, j)) == 2) {
-					print_blue(Type(Unit(M, i, j)));
-					printf(" ");
-				}
-				else {
-					printf("%c ", Type(Unit(M, i, j)));
-				}
-			}
-		}
-		printf("*\n  ");
-		for (j = 0; j <= MapKolEff(M); j++) {
-			printf("*   ");
-		}
-		printf("*\n  ");
-	}
-	for (j = 0; j <= MapKolEff(M); j++) {
-		printf("****");
-	}
-	printf("*\n");
+    for (i=1;i<=MapBrsEff(M);i++) {
+        for (j=1;j<=)
+        printf("")
+    }
 }
 // Mengeprint map pada cmd
