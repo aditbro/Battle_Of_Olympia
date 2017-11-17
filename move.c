656d6146a1323af481162b40e53f11b1314752b1
ayrtoncn <ayrton.niwarlangga@gmail.com>
2017-11-17 20:54:11 +0700

    move.c fix

---
+++ move.c
@@ -0,0 +1,132 @@
+#include "map.h"
+#include "unit.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "ADT/stackt.h"
+#include "ADT/point.h"
+
+Stack X,Y;
+
+
+int absolute(int x){
+	if(x < 0){
+		return -x;
+	}else{
+		return x;
+	}
+}
+
+void call_move()
+/*prosedur ini berguna ketika player memilih untuk change unit dan di awal turn
+ * walaupun player tidak akan menggunakan move
+ *
+ * prosedur ini akan menginisialisasi STACK sehingga bisa melakukan UNDO
+ */
+{
+	stack_CreateEmpty(&X);
+	stack_CreateEmpty(&Y);
+}
+
+void change_unit(UNIT New, UNIT *U)
+/*prosedur ini berguna untuk memilih unit yang akan dipindahkan*/
+{
+	*U = New;
+	call_move();
+}
+
+void possible_move(MAP *P,UNIT U)
+/**prosedur ini mengubah map menjadi map yang sudah berisi
+ * semua langkah-langkah yang mungkin
+ * dilakukan oleh sebuah unit
+ *
+ * diasumsikan pada awalnya di setiap lokasi peta diinisialisasi unit bernilai '0'
+ */
+{
+    int i;
+
+	POINT Loc = Pos(U);
+	for( i = 1; i <= M_Mov(U) && Loc.Y + i <= MapKolEff(*P); i++){
+		if(Type(Unit(*P,Loc.Y + i,Loc.X)) == '0'){
+			Type(Unit(*P,Loc.Y + i,Loc.X)) = '#';
+		}
+		else if(Owner(U) != Owner(Unit(*P,Loc.Y+i,Loc.X))){
+			break;
+		}
+	}
+	for( i = 1; i <= M_Mov(U) && Loc.Y - i >= 0; i++){
+		if(Type(Unit(*P,Loc.Y - i,Loc.X)) == '0'){
+			Type(Unit(*P,Loc.Y - i,Loc.X)) = '#';
+		}
+		else if(Owner(U) != Owner(Unit(*P,Loc.Y-i,Loc.X))){
+			break;
+		}
+	}
+	for( i = 1; i <= M_Mov(U) && Loc.X - i >= 0; i++){
+		if(Type(Unit(*P,Loc.Y,Loc.X - i)) == '0'){
+			Type(Unit(*P,Loc.Y,Loc.X - i)) = '#';
+		}
+		else if(Owner(U) != Owner(Unit(*P,Loc.Y,Loc.X-i))){
+			break;
+		}
+	}
+	for( i = 1; i <= M_Mov(U) && Loc.Y + i <= MapBrsEff(*P); i++){
+		if(Type(Unit(*P,Loc.Y,Loc.X + i)) == '0'){
+			Type(Unit(*P,Loc.Y,Loc.X + i)) = '#';
+		}
+		else if(Owner(U) != Owner(Unit(*P,Loc.Y,Loc.X+i))){
+			break;
+		}
+	}
+	//printMap(P);
+}
+
+void print_possible_move(MAP P, UNIT U)
+/*print semua possible move dari suatu unit*/
+{
+	MAP temp = P;
+	possible_move(&temp, U);
+	printMap(temp);
+}
+
+int check_if_possible(MAP P, UNIT U, int x, int y)
+/*fungsi ini memeriksa apakah sebuah unit bisa pindah ke suatu
+ * koordinat atau tidak, apabila bisa, maka fungsi akan mengembalikan true/1
+ * jika tidak, fungsi akan mengembalikan false/0
+ */
+{
+	MAP temp = P;
+	possible_move(&temp, U);
+	if(Unit(temp, y, x).type != '#'){
+		return 0;
+	}else{
+		return 1;
+	}
+}
+
+void move_unit(MAP *P, UNIT *U, int x, int y)
+/*prosedur ini akan memindahkan unit menuju sebuah sel yang berkoordinat (x,y)
+ * lalu memasukan lokasi sebelumnya ke stack agar bisa melakukan undo
+ */
+{
+	POINT Loc = Pos(*U);
+	Unit(*P, Loc.Y,Loc.X).type = '0';
+	Unit(*P, Loc.Y,Loc.X).owner = 0;
+	stack_Push(&X, Loc.X);
+	stack_Push(&Y, Loc.Y);
+	Loc.X = y;
+	Loc.Y = x;
+	Unit(*P,x,y) = *U;
+	Pos(*U) = Loc;
+	M_Mov(*U) -= 1;
+}
+
+void undo(MAP *P, UNIT *U)
+/*prosedur ini mengembalikan state unit ke state sebelum pindah */
+{
+	int xs;
+	stack_Pop(&X, &xs);
+	int ys;
+	stack_Pop(&Y, &ys);
+	move_unit(P, U, xs, ys);
+	M_Mov(*U) += 2;
+}
