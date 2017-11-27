fd64a0807cbd972c7409e275ba2fe81bc860919a
Osynligare <pinkappleberry98@gmail.com>
2017-11-27 17:19:18 +0700

    Update driver_recruit.c

--- recruit/driver_recruit.c
+++ recruit/driver_recruit.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "recruit.h"
 #include "../offbattle/offbattle.h"
 #include <stdio.h>
@@ -15,21 +14,3 @@ int main() {
 	recruit_unit(&M, &player_1, U);
 	printMap(M);
 }
-=======
-#include "recruit.h"
-#include "../offbattle/offbattle.h"
-#include <stdio.h>
-
-int main() {
-	Player player_1 = create_new_player(1);
-	Player player_2 = create_new_player(2);
-	MAP M = Init_map();
-
-	Init_game(&M, &player_1, &player_2);
-	
-	UNIT U;
-
-	recruit_unit(&M, &player_1, U);
-	printMap(M);
-}
->>>>>>> eeebc0d1b1b157f279ab4aedbb193335bd5a49da
